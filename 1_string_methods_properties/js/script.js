4d591de1aad5d15ca77c73c9a51f11f9993a1f3a
Bhanu Singh <59247235+biasedzeus@users.noreply.github.com>
2022-06-11 21:38:13 +0530

    substring methods

--- 1_string_methods_properties/js/script.js
+++ 1_string_methods_properties/js/script.js
@@ -1,1 +1,62 @@
-console.log("SAPPP")
+// Strings 
+
+const myVar = "NeverGonnaGive"
+const browserType = 'mozilla';
+
+
+// The Length Property
+
+console.log(myVar.length)
+console.log(myVar.charAt(1))
+
+
+// IndexOf Method
+// If the string contains the substring, indexOf() returns the index 
+// of the first occurrence of the substring. If the string does not contain
+//  the substring, indexOf() returns -1.
+
+console.log(myVar.indexOf('Gonna'));
+
+// Last Index Of Method
+
+console.log(myVar.lastIndexOf('e'));
+
+
+// Slice Method
+console.log(myVar.slice(0,5));
+
+
+// to Upper Case Method
+console.log(myVar.toUpperCase());
+
+// to lower Case Method
+console.log(myVar.toLowerCase());
+
+
+
+// includes() method
+console.log(myVar.includes("Never"));
+
+if (browserType.includes('zilla')) {
+  console.log('Found zilla!');
+} else {
+  console.log('No zilla here!');
+
+
+}
+// Startswith And EndsWith Method()
+
+console.log("StartsWith",myVar.startsWith("Never"))
+console.log("EndsWith",myVar.endsWith("Give"))
+
+
+
+
+
+// Split() Method
+console.log(myVar.split("Gonna"))
+
+
+
+
+
