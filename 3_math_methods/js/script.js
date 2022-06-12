6115bdab22cc773fa82ac2401bcd781cfa18432b
Bhanu Singh <59247235+biasedzeus@users.noreply.github.com>
2022-06-12 12:36:49 +0530

    random num gen method

--- 3_math_methods/js/script.js
+++ 3_math_methods/js/script.js
@@ -7,3 +7,17 @@ console.log(sine_val);
 console.log(Math.SQRT2);
 
 console.log("Floor Method",m.floor(234.5));
+
+
+// Returning a random integer between two bounds
+// This can be achieved with a combination
+//  of Math.random() and Math.floor():
+
+function randomGen(min,max){
+    const num = Math.floor(Math.random() * (max- min + 1)) + min;
+
+    return num;
+}
+
+
+console.log(randomGen(1,30));
